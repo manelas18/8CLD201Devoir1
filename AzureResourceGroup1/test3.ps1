92c50258eba656aa90adaccfaa016a1b2760b82e
Carlos BRAZ DA COSTA <manelas18@gmail.com>
2024-11-08 01:09:55 +0100

    Fix encoding issue

---
+++ AzureResourceGroup1/test3.ps1
@@ -0,0 +1,8 @@
+﻿# Lister et supprimer les ressources 
+az resource list --subscription 0fcbc58f-03c0-4daa-9470-840cff312d29 --query "[].id" -o tsv 
+$resources = az resource list --subscription 0fcbc58f-03c0-4daa-9470-840cff312d29 --query "[].id" -o tsv
+foreach ($resource in $resources)
+{ 
+  write-host $resource 
+  #az resource delete --ids $resource
+}
